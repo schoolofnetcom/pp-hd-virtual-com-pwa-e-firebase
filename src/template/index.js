a5356d5dcc617a7b55a4da2011e43e38149721f5
Erik Figueiredo <erik.figueiredo@gmail.com>
2018-03-29 07:09:53 -0300

    semana 2

--- src/template/index.js
+++ src/template/index.js
@@ -13,6 +13,26 @@ module.exports = {
     </div>
     ${sidebar}`,
     action: () => {
-        console.log('done');
+        let openMenu = document.querySelector('#header .menu-icon');
+        let closeMenu = document.getElementById('wrapper');
+
+        let closeMenuEvent = function(e) {
+            e.preventDefault();
+            let body = document.querySelector('body');
+            body.className = body.className.replace('show-menu', '');
+
+            closeMenu.removeEventListener('click', closeMenuEvent, true);
+        };
+
+        let openMenuEvent = function (e) {
+            e.preventDefault();
+            let body = document.querySelector('body');
+            body.className += " show-menu";
+            body.className = body.className.trim();
+
+            closeMenu.addEventListener('click', closeMenuEvent, true);
+        };
+
+        openMenu.addEventListener('click', openMenuEvent);
     }
 }
